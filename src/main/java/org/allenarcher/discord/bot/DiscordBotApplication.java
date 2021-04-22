8d171df42bc9985f5ccfd533beaf1817709df28c
Allen <chunkystyles@gmail.com>
2021-04-22 15:33:35 -0500

    Initial commit

---
+++ src/main/java/org/allenarcher/discord/bot/DiscordBotApplication.java
@@ -0,0 +1,11 @@
+package org.allenarcher.discord.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DiscordBotApplication.class);
+    }
+}
