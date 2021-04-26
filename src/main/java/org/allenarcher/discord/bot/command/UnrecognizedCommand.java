831d2c9f29c5b38da2b19aebab38d26f9e5f2072
Allen <chunkystyles@gmail.com>
2021-04-26 11:48:38 -0500

    More Spring-ification

--- src/main/java/org/allenarcher/discord/bot/command/UnrecognizedCommand.java
+++ src/main/java/org/allenarcher/discord/bot/command/UnrecognizedCommand.java
@@ -1,6 +1,7 @@
 package org.allenarcher.discord.bot.command;
 
 import discord4j.core.object.entity.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -9,11 +10,17 @@ import java.util.List;
 
 @Component
 public class UnrecognizedCommand implements Command {
+    private final String text;
+
+    public UnrecognizedCommand(@Value("${commands.unrecognized.text}") String text) {
+        this.text = text;
+    }
+
     @Override
     public Mono<Void> processCommand(Message message) {
         return Mono.just(message)
                 .flatMap(Message::getChannel)
-                .flatMap((channel -> channel.createMessage("Command '" + message.getContent() + "' is not recognized.")))
+                .flatMap((channel -> channel.createMessage(String.format(text, message.getContent()))))
                 .then();
     }
 
